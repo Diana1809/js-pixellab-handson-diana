c6f749d44fefe155249824366c2548ed05011d5c
Diana1809 <diana.costache1809@gmail.com>
2022-11-29 21:58:48 +0200

    Curs 29.11

---
+++ m01s03/ex03/app.js
@@ -0,0 +1,61 @@
+var person = {
+  name: 'Diana',
+  surname: 'Costache',
+  friends: {},
+};
+
+var larry = {
+  name: 'Larry',
+  surname: 'Larryson',
+  friends: {},
+};
+
+var carol = {
+  name: 'Carol',
+  surname: 'Carolson',
+  friends: {},
+};
+
+var steven = {
+  name: 'Steven',
+  surname: 'Stevenson',
+  friends: {},
+};
+
+var andra = {
+  name: 'Andra',
+  surname: 'Andrason',
+  friends: {},
+};
+
+person.friends.larry = larry;
+person.friends.steven = steven;
+person.friends.andra = andra;
+
+larry.friends.person = person;
+steven.friends.person = person;
+andra.friends.person = person;
+
+larry.friends.steven = steven;
+steven.friends.larry = larry;
+
+steven.friends.andra = andra;
+andra.friends.steven = steven;
+
+carol.friends.andra = andra;
+andra.friends.carol = carol;
+
+delete larry.friends.person;
+delete person.friends.larry;
+
+console.log(
+  larry.friends.steven.friends.person.name +
+    ' ' +
+    larry.friends.steven.friends.person.surname,
+);
+
+console.log(
+  carol.friends.andra.friends.person.name +
+    ' ' +
+    carol.friends.andra.friends.person.surname,
+);
